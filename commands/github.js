e190f003a5b5f4985c5dd529e5fa3fe793420e01
Itai Kimelman <itaikimelman@Itais-MacBook-Air.local>
2022-08-14 10:26:39 +0300

    github command now works

--- commands/github.js
+++ commands/github.js
@@ -1,10 +1,15 @@
 const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
 
+const ghLink = new EmbedBuilder()
+    .setColor(0x003366)
+    .setTitle('Link to the source code of the bot')
+    .setURL('https://github.com/24kimel/kimelbot')
+    .setDescription('Take a look at the implementation of Kimelbot');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('github')
         .setDescription('get link to github repo'),
     async execute(interaction) {
-        
+        await interaction.reply({content: '', embeds: [ghLink]});
     }
 };
